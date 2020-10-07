455c288c4bc656bcb848dc26b6f066623261aafc
Q-R-Code <qruffier@gmail.com>
2020-10-07 16:33:02 +0200

    aaa

--- POO/la_poo_avec_python-00_setup/model.py
+++ POO/la_poo_avec_python-00_setup/model.py
@@ -1,20 +1,28 @@
+import json
 
 class Agent:
 
-    def say_hello(self, first_name):
-        return "Bien le bonjour " + first_name + "!"
-
-    def __init__(self, agent_attributes):
+    def __init__(self, position,  **agent_attributes):
+        self.position = position
         for attr_name, attr_value in agent_attributes.items():
             setattr(self, attr_name, attr_value)
 
+class Position:
+    def __init__(self, longitude, latitude):
+        self.longitude = longitude
+        self.latitude = latitude
 
 
-agent_attributes = {"neuroticism": -0.0739192627121713, "language": "Shona", "latitude": -19.922097800281783,
-                    "country_tld": "zw", "age": 12, "income": 333, "longitude": 29.798455535838603, "sex": "Male",
-                    "religion": "syncretic", "extraversion": 1.051833688742943, "date_of_birth": "2005-01-10",
-                    "agreeableness": 0.1441229877537559, "id_str": "LB3-3Cl", "conscientiousness": 0.2419104411765549,
-                    "internet": "false", "country_name": "Zimbabwe", "openness": -0.024607605122172617, "id": 6636726630}
+def main():
+    for agent_attributes in json.load(open("agents-100k.json")):
+        latitude = agent_attributes.pop("latitude")
+        longitude = agent_attributes.pop("longitude")
+        position = Position(longitude, latitude)
+        agent = Agent(position, **agent_attributes)
+        print(agent.position.latitude)
 
-agent = Agent(agent_attributes)
-print(agent.extraversion)
+
+main()
+
+
+
